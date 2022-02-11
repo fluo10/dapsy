880e0ac6627a10c1afbd4527b1c193f90bf75aad
fluo10 <fluo10.dev@fireturtle.net>
2022-02-11 16:21:48 +0900

    Fix args

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 mod args;
 mod cmd;
 
-use clap::{Parser, Subcommand};
+use clap::{Args,ArgGroup,Parser, Subcommand};
 use std::path::PathBuf;
 pub use args::GlobalArgs;
 use cmd::MainSubcommand;
@@ -9,13 +9,40 @@ use cmd::MainSubcommand;
 
 #[derive(Parser)]
 #[clap(about, version, author)]
+//#[clap(group(
+//    ArgGroup::new("files").arg("source").arg("destination").required(true).multiple(true),
+//))]
+//#[clap(group(
+//    ArgGroup::new("mode").arg("preset").conflicts_with("files"),
+//))]
 pub struct MainCommand {
+    #[clap(short, group = "test")]
+    preset: bool,
+    #[clap()]
+    source: Option<PathBuf>,
+    #[clap()]
+    destination: Option<PathBuf>,
     #[clap(subcommand)]
-    pub subcommand: MainSubcommand,
+    pub subcommand: Option<MainSubcommand>,
+
 }
+
 
 impl MainCommand {
     pub fn run(&self) {
-        self.subcommand.run();
+        if let Some(x) = &self.subcommand {
+            x.run();
+            ()
+        };
+        let src = self.source.as_ref().unwrap();
+        let dst = self.destination.as_ref().unwrap().clone();
+        println!("Convert {} to {}", src.to_str().unwrap(), dst.to_str().unwrap());
+
     }
+}
+
+#[derive(Args)]
+pub struct FileArgs {
+    pub source: PathBuf,
+    pub destination: PathBuf,
 }
--- src/cli/cmd/track/convert.rs
+++ src/cli/cmd/track/convert.rs
@@ -4,6 +4,8 @@ use std::path::{Path,PathBuf};
 
 #[derive(Parser)]
 pub struct TrackConvertCommand {
+    #[clap(short='n',long)]
+    dry_run: bool,
     source: PathBuf,
     destination: PathBuf,
 }
@@ -11,5 +13,14 @@ pub struct TrackConvertCommand {
 impl TrackConvertCommand{
     pub fn run(&self) {
         println!("Run track convert");
+        let mut source_files:Vec<PathBuf> = Vec::new();
+        if self.source.is_file() {
+            source_files.push(self.source.clone());
+        } else if self.source.is_dir() {
+            
+        }
+        if self.dry_run {
+            println!("Convert from")
+        }
     }
 }
