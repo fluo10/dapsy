dfbbba548b43e0b292283bcb9e2f509fc742dee3
fluo10 <fluo10.dev@fireturtle.net>
2022-02-15 08:30:35 +0900

    Update config parser

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,30 @@
 use std::collections::{HashMap,HashSet};
 use std::path::PathBuf;
 use once_cell::sync::OnceCell;
+use serde::{Serialize, Deserialize};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 static INSTANCE: OnceCell<Config> = OnceCell::new();
 
-#[derive(Debug)]
+
+
+#[serde(default)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Config {
     pub lossy_formats: HashSet<String>,
     pub lossless_formats: HashSet<String>,
     pub audio_formats: HashSet<String>,
     pub presets: HashMap<String,Preset>,
+    #[serde(skip)]
     pub dry_run: bool,
+    #[serde(skip)]
     pub verbose: bool,
 }
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct Preset {
-    pub src_path: PathBuf,
-    pub dst_path: PathBuf,
+    pub source: PathBuf,
+    pub destination: PathBuf,
     pub playlist_dir: Option<PathBuf>,
 }
 impl Config {
@@ -27,6 +33,19 @@ impl Config {
     }
     pub fn globalize(self) {
         INSTANCE.set(self).unwrap();
+    }
+
+    pub fn from_toml(t: &str) -> Self {
+        toml::from_str(t).unwrap()
+    }
+    pub fn to_toml(&self) -> String {
+        toml::to_string(&self).unwrap()
+    }
+    pub fn read(path: &impl AsRef<PathBuf>) {
+
+    }
+    pub fn write(&self, path: &impl AsRef<PathBuf>) {
+
     }
 }
 impl Default for Config {
@@ -49,4 +68,24 @@ impl Default for Config {
         
     }
     
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn deserialize_toml() {
+        const TOML: &str = r#"[presets.test_preset]
+        source= "path/to/source/dir"
+        destination = "path/to/destination/dir"
+        "#;
+        let config_from_toml = Config::from_toml(TOML);
+        let mut config = Config::default();
+        config.presets.insert( "test_preset".to_string(), Preset{
+            source: PathBuf::from("path/to/source/dir"),
+            destination : PathBuf::from("path/to/destination/dir"),
+            playlist_dir: None,
+        });
+        assert_eq!(config_from_toml, config);
+    }
 }
